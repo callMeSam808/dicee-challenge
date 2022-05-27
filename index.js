a1753a1fe0ba17554510d9e2619dcc958eb1c2d2
Oshamaru <callMeSam808@gmail.com>
2022-05-27 12:37:58 -1000

    Add random behavior to src of second dice. Modify header to display result.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 let randomNumber1 = Math.floor((Math.random() * 6) + 1);
-console.log(randomNumber1);
 
 let dice1 = document.querySelector(".img1");
 dice1.src = "images/dice"+randomNumber1+".png";
+
+let randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+let dice2 = document.querySelector(".img2");
+dice2.src = "images/dice"+randomNumber2+".png";
+
+let header = document.querySelector("h1");
+let result;
+if (randomNumber1 > randomNumber2) {
+  result = "Player 1 wins!"
+}
+else if (randomNumber1 < randomNumber2) {
+  result = "Player 2 wins!"
+}
+else {
+  result = "Draw!"
+}
+header.textContent = result;
